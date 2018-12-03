565dd17b79ddb0142147698803370054ca5a757d
Yoshiki-Yamada <yoshiki1124.kobe24@gmail.com>
2018-12-03 12:29:59 +0900

    first commit

---
+++ src/sample1/Sample1.java
@@ -0,0 +1,26 @@
+package sample1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+
+public class Sample1 extends Application{
+    private Label lb;
+    public static void main(String[] args){
+        launch(args);
+    }
+    public void start(Stage stage)throws Exception{
+        lb = new Label();
+        lb.setText("いらっしゃいませ。");
+        BorderPane bp = new BorderPane();
+        bp.setCenter(lb);
+        Scene sc = new Scene(bp,300,200);
+        stage.setScene(sc);
+        stage.setTitle("サンプル");
+        stage.show();
+    }
+}
